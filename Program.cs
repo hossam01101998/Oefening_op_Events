be4e11bd256ced9cc2e52d3acc1871ae08486133
hossam01101998 <hossam1998hossam@gmail.com>
2023-10-26 21:16:55 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using Oefening_op_Events;
+
+class Program
+{
+    static void Main()
+    {
+        Stapel<string> stapel = new Stapel<string>();
+        stapel.InhoudGewijzigd += (item, wijziging) =>
+        {
+            switch (wijziging)
+            {
+                case WijzigingType.Toevoeging:
+                    Console.WriteLine($"Item toegevoegd: {item}");
+                    break;
+                case WijzigingType.Verwijdering:
+                    Console.WriteLine($"Item verwijderd: {item}");
+                    break;
+                case WijzigingType.Leegmaking:
+                    Console.WriteLine("Stapel geleegd");
+                    break;
+            }
+        };
+        //last in first out. Het gaat de laatste toegevoegde item verwijderen.
+
+        stapel.Toevoegen("Volkswagen");
+        stapel.Toevoegen("Mazda");
+        stapel.Toevoegen("BMW");
+        stapel.Verwijderen();
+        stapel.Leegmaken();
+    }
+}
---
+++ Stapel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oefening_op_Events
+{
+    internal class Stapel<T>
+    {
+        private List<T> stapel = new List<T>();
+
+        public event Action<T, WijzigingType> InhoudGewijzigd;
+
+        public void Toevoegen(T item)
+        {
+
+            stapel.Add(item);
+            OnInhoudGewijzigd(item, WijzigingType.Toevoeging);
+        }
+
+        public T Verwijderen()
+        {
+            if (stapel.Count > 0)
+            {
+
+                T verwijderdItem = stapel[stapel.Count - 1];
+                stapel.RemoveAt(stapel.Count - 1);
+                OnInhoudGewijzigd(verwijderdItem, WijzigingType.Verwijdering);
+                return verwijderdItem;
+            }
+            else
+            {
+                throw new InvalidOperationException("De stapel is leeg, kan geen items verwijderen.");
+            }
+        }
+
+        public void Leegmaken()
+        {
+            stapel.Clear();
+
+            OnInhoudGewijzigd(default(T), WijzigingType.Leegmaking);
+        }
+
+        protected virtual void OnInhoudGewijzigd(T item, WijzigingType wijziging)
+        {
+            InhoudGewijzigd?.Invoke(item, wijziging);
+        }
+    }
+
+    // de drie opties
+    enum WijzigingType
+    {
+        Toevoeging,
+        Verwijdering,
+        Leegmaking
+    }
+
+}
